259f3b7ef86bf46e1135ce0e43f2554f489af8cc
orca1018 <dev@jiraroj.net>
2025-05-18 19:27:34 +0700

    update mix.exs, get ready to make it public

--- mix.exs
+++ mix.exs
@@ -7,22 +7,36 @@ defmodule AOC2015.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Advent of Code 2015 (Elixir)",
+      description:
+        "My solution on Advent of Code 2015 in Elixir, with test scripts and AI-generated story summaries.",
+      source_url: "https://github.com/jiraroj-wir/AoC-2015-elixir",
+      homepage_url: "https://github.com/jiraroj-wir/AoC-2015-elixir",
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Unlicense"],
+      maintainers: ["jiraroj-wir"],
+      links: %{
+        "GitHub" => "https://github.com/jiraroj-wir/AoC-2015-elixir"
+      }
     ]
   end
 end
