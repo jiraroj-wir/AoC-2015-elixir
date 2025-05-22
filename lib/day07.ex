d01e9bab7e097e16057df06155b705ae9696e717
orca1018 <dev@jiraroj.net>
2025-05-22 22:59:19 +0700

    day 7 - part 1, I knew lazy evaluation, but it challanges you different in elixir: parameter passing, recursion stuff, not as confusing as ragex modules though

---
+++ lib/day07.ex
@@ -0,0 +1,82 @@
+defmodule AOC2015.Day07 do
+  import Bitwise
+
+  @doc """
+  read the puzzle input file
+  """
+  def input do
+    File.read!("priv/inputs/day07.txt")
+  end
+
+  @doc """
+  wrapper around part1, reads the puzzle input and return the answer for part 1
+  """
+  def part1(), do: part1(input(), "a")
+
+  def part1(input, target) do
+    instruction_map =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [expr, target] = String.split(line, " -> ", trim: true)
+        {String.trim(target), String.trim(expr)}
+      end)
+      |> Map.new()
+
+    {value, _cache} = lazy_evaluate(instruction_map, target)
+    value
+  end
+
+  def lazy_evaluate(instructions, wire, cache \\ %{}) do
+    cond do
+      is_number_literal(wire) ->
+        {String.to_integer(wire), cache}
+
+      Map.has_key?(cache, wire) ->
+        {Map.get(cache, wire), cache}
+
+      true ->
+        expr = Map.fetch!(instructions, wire)
+        {value, new_cache} = evaluate_expression(expr, instructions, cache)
+        {value, Map.put(new_cache, wire, value)}
+    end
+  end
+
+  defp is_number_literal(str) do
+    case Integer.parse(str) do
+      {_, ""} -> true
+      _ -> false
+    end
+  end
+
+  defp evaluate_expression(expr, instructions, cache) do
+    parts = String.split(expr, " ")
+
+    case parts do
+      [a] ->
+        lazy_evaluate(instructions, a, cache)
+
+      ["NOT", x] ->
+        {val, c} = lazy_evaluate(instructions, x, cache)
+        {~~~val &&& 0xFFFF, c}
+
+      [a, "AND", b] ->
+        {va, c1} = lazy_evaluate(instructions, a, cache)
+        {vb, c2} = lazy_evaluate(instructions, b, c1)
+        {va &&& vb, c2}
+
+      [a, "OR", b] ->
+        {va, c1} = lazy_evaluate(instructions, a, cache)
+        {vb, c2} = lazy_evaluate(instructions, b, c1)
+        {va ||| vb, c2}
+
+      [a, "LSHIFT", n] ->
+        {va, c} = lazy_evaluate(instructions, a, cache)
+        {va <<< String.to_integer(n), c}
+
+      [a, "RSHIFT", n] ->
+        {va, c} = lazy_evaluate(instructions, a, cache)
+        {va >>> String.to_integer(n), c}
+    end
+  end
+end
---
+++ test/day07_test.exs
@@ -0,0 +1,36 @@
+defmodule AOC2015.Day07Test do
+  use ExUnit.Case
+  alias AOC2015.Day07
+
+  describe "part1/1" do
+    test "returns total wrapping paper's area" do
+      input = """
+        123 -> x
+        456 -> y
+        x AND y -> d
+        x OR y -> e
+        x LSHIFT 2 -> f
+        y RSHIFT 2 -> g
+        NOT x -> h
+        NOT y -> i
+      """
+
+      assert Day07.part1(input, "d") == 72
+      assert Day07.part1(input, "e") == 507
+      assert Day07.part1(input, "f") == 492
+      assert Day07.part1(input, "g") == 114
+      assert Day07.part1(input, "h") == 65412
+      assert Day07.part1(input, "i") == 65079
+      assert Day07.part1(input, "x") == 123
+      assert Day07.part1(input, "y") == 456
+    end
+  end
+
+  """
+  describe "part2/1" do
+    test "returns total ribbon length" do
+      assert Day02.part2("1x1x10") == 14
+    end
+  end
+  """
+end
